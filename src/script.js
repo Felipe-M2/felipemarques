05fd55a43f00b4fa6c14d0fcf3150863f71c03af
felipe <felipemarquescosta8@gmail.com>
2023-02-08 12:07:42 -0300

    Corrigido bugs e adicionado fotos ao portfolio

--- src/script.js
+++ src/script.js
@@ -1,14 +1,20 @@
 let btnOrcamentoForm = document.querySelector(".btnOrcamentoForm");
+let labelForm = document.querySelector(".labelForm");
 
 let faseForm = 1;
+
+nomeCliente = "";
 
 let formFaseFunction = ()=>{
     switch(faseForm){
         case 1:
+            let nomeForm = document.querySelector("#nomeOrcamento").value;
             document.querySelector(".nameForm").style.display = "none";
             document.querySelector(".projetoForm").style.display = "flex";
             document.querySelector(".fase2").style.opacity = "1";
             document.querySelector(".fase1").style.opacity = "0.5";
+            nomeCliente = nomeForm.value;
+            labelForm.innerHTML = `Olá ${nomeForm}, Selecione qual tipo de projeto você quer:`;
             faseForm = faseForm + 1;
 
             break;
@@ -18,6 +24,7 @@ let formFaseFunction = ()=>{
             document.querySelector(".contatoForm").style.display = "flex";
             document.querySelector(".fase3").style.opacity = "1";
             document.querySelector(".fase2").style.opacity = "0.5";
+            labelForm.innerHTML = "Otimo, digita o seu telefone:";
 
             faseForm = faseForm + 1;
             break;
@@ -27,6 +34,7 @@ let formFaseFunction = ()=>{
             document.querySelector(".emailForm").style.display = "flex";
             document.querySelector(".fase4").style.opacity = "1";
             document.querySelector(".fase3").style.opacity = "0.5";
+            labelForm.innerHTML = "Qual o seu email?";
 
             faseForm = faseForm + 1;
             break;
@@ -36,6 +44,7 @@ let formFaseFunction = ()=>{
             document.querySelector(".infoClient").style.display = "flex";
             document.querySelector(".fase5").style.opacity = "1";
             document.querySelector(".fase4").style.opacity = "0.5";
+            labelForm.innerHTML = "Me explica como você quer o seu projeto:";
 
             faseForm = faseForm + 1;
             break;
@@ -43,6 +52,8 @@ let formFaseFunction = ()=>{
         case 5:
             document.querySelector(".infoClient").style.display = "none";
             document.querySelector(".fase5").style.opacity = "0.5";
+
+            labelForm.innerHTML = "Pronto só aguardar nosso contato!";
             break;
     }
 }
